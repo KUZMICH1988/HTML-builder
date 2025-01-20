52b408ff0b054707cbac80fa1a3eabf989c2178c
KuzmichAleksandr <fnt111333@gmail.com>
2025-01-20 14:00:11 +0300

    feat: solve 5th task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+
+// Path to the folders
+const stylesPath = path.join(__dirname, 'styles');
+const projectPath = path.join(__dirname, 'project-dist');
+const bundlePath = path.join(projectPath, 'bundle.css');
+
+// Create or clean bundle.css
+fs.writeFileSync(bundlePath, '');
+
+// Read folder 'styles'
+fs.readdir(stylesPath, (err, files) => {
+  if (err) {
+    console.error("Ошибка чтения папки", err);
+    return;
+  }
+
+  // filtr only CSS files
+  const cssFiles = files.filter(file => path.extname(file) === '.css');
+
+
+  // read and integrate css files
+  cssFiles.forEach(file => {
+    const filePath = path.join(stylesPath, file);
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        console.error("Ошибка чтения файла", err);
+        return;
+      }
+
+      // Write everything in bundle.css
+      fs.appendFile(bundlePath, data + '\n', err => {
+        if (err) {
+          console.error("Ошибка записи файла", err);
+          return;
+        }
+      })
+    })
+
+  })
+})
+
+
+
+
+// console.log(stylesPath);
+// console.log(projectPath);
+// console.log(bundlePath);
+// console.log(cssFiles);
